e01ef33a0028bfdbf7eb79aff062421ee0a92608
Luna <52232518+Lunaware@users.noreply.github.com>
2024-03-12 14:28:39 +1300

    Update version and signal names

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * @package prettier-zones
  * @author methamphetqmine
- * @version 1.0.0
+ * @version 1.0.1
  */
 
 import { Workspace, CollectionService, Players } from "@rbxts/services";
@@ -20,9 +20,9 @@ export class Zone {
 	private overlapParams: OverlapParams = new OverlapParams();
 	private Maid: Trove = new Trove();
 
-	public TagAdded: Signal<BasePart> = new Signal();
-	public PlayerEntered: Signal<Player> = new Signal();
-	public PlayerExited: Signal<Player> = new Signal();
+	public tagAdded: Signal<BasePart> = new Signal();
+	public playerEntered: Signal<Player> = new Signal();
+	public playerExited: Signal<Player> = new Signal();
 
 	constructor(Options: { Tag: string; queryTime: number }) {
 		assert(typeOf(Options) === "table", "Expected table for argument #1, got nil.");
@@ -35,18 +35,28 @@ export class Zone {
 		this.Options.Tag = Options.Tag;
 		this.Options.queryTime = Options.queryTime !== undefined ? Options.queryTime : 0.25;
 
-		this.Maid.connect(this.TagAdded, (Object: BasePart) => {
+		this.Maid.connect(this.tagAdded, (Object: BasePart) => {
 			while (Object.Parent) {
+				const thisQuery: Array<Player> = [];
+
 				Workspace.GetPartsInPart(Object, this.overlapParams).forEach((Part: BasePart) => {
 					const Ancestor = Part.FindFirstAncestorOfClass("Model");
 					const Humanoid = Ancestor?.FindFirstChildOfClass("Humanoid");
 					const Player = Humanoid && Players.GetPlayerFromCharacter(Ancestor);
 
 					if (Player !== undefined && !this.lastQuery.includes(Player)) {
-						this.PlayerEntered.Fire(Player);
+						thisQuery.push(Player);
+						this.playerEntered.Fire(Player);
 					}
 				});
 
+				this.lastQuery.forEach((Player: Player) => {
+					if (!thisQuery.includes(Player)) {
+						this.playerExited.Fire(Player);
+					}
+				});
+
+				this.lastQuery = thisQuery;
 				task.wait(this.Options.queryTime);
 			}
 		});
@@ -54,14 +64,14 @@ export class Zone {
 		for (const Object of CollectionService.GetTagged(this.Options.Tag)) {
 			if (Object.IsA("BasePart") === true) {
 				this.Objects.push(Object);
-				this.TagAdded.Fire(Object as BasePart);
+				this.tagAdded.Fire(Object as BasePart);
 			}
 		}
 
 		this.Maid.connect(Workspace.ChildAdded, (Object: Instance) => {
 			if (Object.HasTag(this.Options.Tag) && Object.IsA("BasePart") === true) {
 				this.Objects.push(Object);
-				this.TagAdded.Fire(Object as BasePart);
+				this.tagAdded.Fire(Object as BasePart);
 			}
 		});
 	}
